77ea136e9ece364a07378bf706d1b74808ca72e7
cc <chongwangcc@gmal.com>
2019-01-20 19:36:04 +0800

    +功能：host设定为0.0.0.0

--- person_time_manage_system/app.py
+++ person_time_manage_system/app.py
@@ -78,6 +78,5 @@ def index(user_name):
     return render_template("index.html")
 
 
-
 if __name__ == "__main__":
-    app.run(debug=True, port=5001)
+    app.run(debug=True, host="0.0.0.0", port=5001)
