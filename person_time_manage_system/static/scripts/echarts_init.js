20b22b2add67b1fe5342e02a1518258d57ca87b2
cc <chongwangcc@gmail.com>
2019-05-06 15:49:17 +0800

    [+功能] 更换仪表盘

--- person_time_manage_system/static/scripts/echarts_init.js
+++ person_time_manage_system/static/scripts/echarts_init.js
@@ -1,6 +1,6 @@
 
 //仪表盘初始化
-function dashboard_init(id_str, data1){
+function dashboard_init2(id_str, data1){
     // 番茄始终达标率
     var nums = data1
     //设置番茄始终达标数
@@ -335,6 +335,135 @@ function dashboard_init(id_str, data1){
     }]
     });
 }
+
+
+function dashboard_init(id_str, data1){
+    var nums = data1;
+    nums = 20
+    option = {
+          backgroundColor: "#062a44",
+          series: [{
+            name: '刻度',
+            type: 'gauge',
+            radius: '88%',
+            min:0,//最小刻度
+            max:100,//最大刻度
+            splitNumber: 10, //刻度数量
+            startAngle: 225,
+            endAngle: -45,
+            axisLine: {
+              show: true,
+              lineStyle: {
+                width: 1,
+                color: [[1,'rgba(0,0,0,0)']]
+              }
+            },//仪表盘轴线
+            axisLabel: {
+              show: true,
+              color:'#4d5bd1',
+              distance:25,
+            },//刻度标签。
+            axisTick: {
+              show: true,
+               splitNumber: 5,
+              lineStyle: {
+                // color: '#fff',
+                width: 1,
+              },
+              length: -8
+            },//刻度样式
+            splitLine: {
+              show: true,
+              length: -20,
+              lineStyle: {
+                color: '#fff'
+              }
+            },//分隔线样式
+            detail: {
+              show: false
+            },
+            pointer: {
+              show: false
+            }
+          },{
+              type: 'gauge',
+              radius: '75%',
+              center: ['50%', '50%'],
+
+              splitNumber: 0, //刻度数量
+              startAngle: 225,
+              endAngle: -45,
+              axisLine: {
+                show: true,
+                lineStyle: {
+                  width: 15,
+                  color: [
+                    [
+                      nums/100, new echarts.graphic.LinearGradient(
+                      0, 0, 1, 0, [{
+                      offset: 0,
+                      color: '#5c53de'
+                    },
+                      {
+                        offset: 1,
+                        color: '#18c8ff'
+                      }
+                    ]
+                      )
+                    ],
+                    [
+                      1, '#413e54'
+                    ]
+                  ]
+                }
+              },
+              //分隔线样式。
+              splitLine: {
+                show: false,
+              },
+              axisLabel: {
+                show: false
+              },
+              axisTick: {
+                show: false
+              },
+              pointer: {
+                show: false
+              },
+              title: {
+                show: true,
+                offsetCenter: [0, '-26%'], // x, y，单位px
+                textStyle: {
+                  color: '#fff',
+                  fontSize: 20
+                }
+              },
+            //仪表盘详情，用于显示数据。
+            detail: {
+                show: true,
+                offsetCenter: [0, '16%'],
+                color: '#ffffff',
+                formatter: function(params) {
+                  return params
+                },
+                textStyle: {
+                  fontSize: 44
+                }
+            },
+            data: [{
+                name: "番茄时钟数",
+                value: nums
+                }]
+          }
+          ]
+        };
+
+    var pieChart1 = echarts.init(document.getElementById(id_str));
+
+    pieChart1.setOption(option)
+
+}
+
 
 //柱状图
 function bar_init(id_str, data){
