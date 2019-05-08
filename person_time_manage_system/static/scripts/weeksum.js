8ba814d5d5905546a4554b827ec4eff48b0b3ffc
chongwangcc <chongwangcc@gmail.com>
2019-05-09 06:15:14 +0800

    [-bug] 添加 google帐号登出功能

--- person_time_manage_system/static/scripts/weeksum.js
+++ person_time_manage_system/static/scripts/weeksum.js
@@ -189,3 +189,19 @@ function init5(id_str, data){
          }
 }
 
+
+function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+      console.log('User signed out.');
+
+      window.location.href="/logout"
+    });
+}
+
+function onLoad() {
+  gapi.load('auth2', function() {
+    gapi.auth2.init();
+  });
+}
+
